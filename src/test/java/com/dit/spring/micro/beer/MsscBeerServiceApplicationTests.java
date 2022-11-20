99398ba9ae713acc7cbace65530ad140ddc999b9
Daniel Xu <zhiqiang.xu2015@gmail.com>
2022-11-20 12:50:35 -0500

    Initial project add. Closes #1

---
+++ src/test/java/com/dit/spring/micro/beer/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dit.spring.micro.beer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
